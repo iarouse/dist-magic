6847715a9f75f74f1434123bede00233ba5ad094
gudh <siloyiwa@gmail.com>
2014-11-10 02:31:58 -0500

    The new v1

--- v1/scripts/app.js
+++ v1/scripts/app.js
@@ -14,6 +14,7 @@
         'ngTagsInput',
         'textAngular',
         'angular-loading-bar',
+        'ui.calendar',
 
         // Custom modules
         'app.nav',
@@ -25,7 +26,8 @@
         'app.ui.map',
         'app.page',
         'app.table',
-        'app.task'
+        'app.task',
+        'app.calendar'
     ]);
 
 })();
@@ -54,7 +56,7 @@
                 'charts/charts', 'charts/flot',
                 'pages/404', 'pages/500', 'pages/blank', 'pages/forgot-password', 'pages/invoice', 'pages/lock-screen', 'pages/profile', 'pages/invoice', 'pages/signin', 'pages/signup',
                 'mail/compose', 'mail/inbox', 'mail/single',
-                'tasks/tasks'
+                'app/tasks', 'app/calendar'
             ]
 
             setRoutes = function(route) {
@@ -1168,8 +1170,8 @@
                 return getRandomData;    
             }
 
-            getRandomData1 = makeGetRandomData(data1, 25, 45);
-            getRandomData2 = makeGetRandomData(data2, 55, 75);
+            getRandomData1 = makeGetRandomData(data1, 28, 42);
+            getRandomData2 = makeGetRandomData(data2, 56, 72);
 
 
             update = function() {
@@ -2294,6 +2296,126 @@
         }
     }
 })(); 
+;
+(function () {
+    'use strict';
+
+    angular.module('app.calendar', ['ui.calendar', 'ui.bootstrap']);
+
+})();
+
+;
+(function () {
+    'use strict';
+
+    angular.module('app.calendar')
+        .controller('calendarCtrl', [ '$scope', calendarCtrl]);
+
+    function calendarCtrl($scope) {
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
+        
+        /* event source that contains custom events on the scope */
+        $scope.events = [
+            {title: 'All Day Event',start: new Date(y, m, 1)},
+            {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
+            {title: 'Go Hiking',start: new Date(y, m, d - 1), className: ['fc-event-warning']},
+            {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false, className: ['fc-event-success']},
+            {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false, className: ['fc-event-success']},
+            {title: 'Birthday Party',start: new Date(y, m, d + 1, 11, 0),end: new Date(y, m, d + 1, 12, 30),allDay: false, className: ['fc-event-danger']},
+            {title: 'Shopping',start: new Date(y, m, d + 2, 9, 0),end: new Date(y, m, d + 2, 12, 0),allDay: false, className: ['fc-event-success']},
+            {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'},
+            {title: 'Shopping',start: new Date(y, m + 1, 8)},
+        ];
+        /* event source that calls a function on every view switch */
+        $scope.eventsF = function (start, end, timezone, callback) {
+            var s = new Date(start).getTime() / 1000;
+            var e = new Date(end).getTime() / 1000;
+            var m = new Date(start).getMonth();
+            var events = [{title: 'Feed Me ' + m,start: s + (50000),end: s + (100000),allDay: false, className: ['customFeed']}];
+            callback(events);
+        };
+
+        $scope.calEventsExt = {
+             color: '#f00',
+             textColor: 'yellow',
+             events: [ 
+                    {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
+                    {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
+                    {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+                ]
+        };
+        /* alert on eventClick */
+        $scope.alertOnEventClick = function( event, allDay, jsEvent, view ){
+                $scope.alertMessage = (event.title + ' was clicked ');
+        };
+        /* alert on Drop */
+         $scope.alertOnDrop = function( event, revertFunc, jsEvent, ui, view){
+             $scope.alertMessage = ('Event Droped on ' + event.start.format());
+        };
+        /* alert on Resize */
+        $scope.alertOnResize = function( event, jsEvent, ui, view){
+             $scope.alertMessage = ('Event end date was moved to ' + event.end.format());
+        };
+        /* add and removes an event source of choice */
+        $scope.addRemoveEventSource = function(sources,source) {
+            var canAdd = 0;
+            angular.forEach(sources,function(value, key){
+                if(sources[key] === source){
+                    sources.splice(key,1);
+                    canAdd = 1;
+                }
+            });
+            if(canAdd === 0){
+                sources.push(source);
+            }
+        };
+        /* add custom event*/
+        $scope.addEvent = function() {
+            $scope.events.push({
+                title: 'New Event',
+                start: new Date(y, m, d),
+                end: new Date(y, m, d + 1)
+            });
+        };
+        /* remove event */
+        $scope.remove = function(index) {
+            $scope.events.splice(index,1);
+        };
+        /* Change View */
+        $scope.changeView = function(view,calendar) {
+            calendar.fullCalendar('changeView',view);
+        };
+        /* Change View */
+        $scope.renderCalender = function(calendar) {
+            if(calendar){
+                calendar.fullCalendar('render');
+            }
+        };
+        /* config object */
+        $scope.uiConfig = {
+            calendar:{
+                height: 450,
+                editable: true,
+                header:{
+                    left: 'title',
+                    center: '',
+                    right: 'today prev,next'
+                },
+                eventClick: $scope.alertOnEventClick,
+                eventDrop: $scope.alertOnDrop,
+                eventResize: $scope.alertOnResize
+            }
+        };
+
+        /* event sources array*/
+        $scope.eventSources = [$scope.events, $scope.eventsF];
+    }
+
+})();
+
 ;
 (function () {
     'use strict';
